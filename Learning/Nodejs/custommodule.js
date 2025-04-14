d5bba7cfda6fe7ce37c711ce1ff91f3adaf6a025
Dhanush M <115058565+DhanushManickam@users.noreply.github.com>
2025-04-14 19:11:57 +0530

    File and custom module declearion

---
+++ Learning/Nodejs/custommodule.js
@@ -0,0 +1,15 @@
+function  add(a , b){
+    return a + b;
+}
+
+function subract (a, b){
+    return a - b;
+}
+
+function multiple(a , b){
+    return a * b;
+}
+
+module.exports = {
+    add, subract, multiple
+}
