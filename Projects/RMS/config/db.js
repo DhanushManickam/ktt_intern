1991edfb2ca35d17b20371fbdfe0b9a010703a14
[DhanushManickam] <[2k21cse017@kiot.ac.in]>
2025-05-31 16:45:10 +0530

    Created new repo for project

--- Projects/RMS/config/db.js
+++
@@ -1,8 +0,0 @@
-const {Sequelize} = require('sequelize');
-
-const sequelize = new Sequelize('rms', 'postgres', 'kiot2003',{
-    host : 'localhost',
-    dialect : 'postgres'
-})
-
-module.exports = sequelize;
